bab6aff316a67e8436f6961e59d1294b8263732c
Sotnikov Oleksii <a.sotnikov.b@gmail.com>
2021-08-06 21:36:52 +0300

    rite1

--- src/Master.java
+++ src/Master.java
@@ -2,5 +2,6 @@ public class Master {
     public static void main(String[] args) {
         System.out.println("this time it should work");
         System.out.println("if it doesn't work out again now, I'll go to bed. :P");
+        System.out.println("начал делать правую ветку");
     }
 }
